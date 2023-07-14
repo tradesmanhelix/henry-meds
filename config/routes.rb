e029f8571df92a0e2155e93594ee17fdd3bbfd5c
Alex Smith <alexmcraysmith@gmail.com>
2023-07-14 12:52:25 -0600

    Create API Controller for Provider Schedules

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :providers, only: [] do
+        resources :schedules, only: [:index, :create] do
+          post 'confirm'
+          post 'reserve'
+        end
+      end
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
